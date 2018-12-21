4ea3bb24ecd929d8495abfc8369690edbe7e11e9
srilakshmi-malineni <srilakshmi529@gmail.com>
2018-12-21 11:08:39 +0530

    modified base class

--- src/org/git1/sample.java
+++ src/org/git1/sample.java
@@ -3,5 +3,11 @@ package org.git1;
 public class sample {
 public static void main(String[] args) {
 	System.out.println("Hello,This is my First git project");
+	sample s=new sample();
+	s.test1();
+	
+}
+public void test1() {
+System.out.println("Hello");
 }
 }
